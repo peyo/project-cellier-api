2a9b1ee7c0e886a0b5c18770d881385e7b94b40e
peyo <peteryyoon@gmail.com>
2020-07-08 14:51:43 -0700

    updates to test data

--- test/data/test-users.js
+++ test/data/test-users.js
@@ -1,35 +1,35 @@
 let TestUsers = [
   {
     id: 1,
-    username: "peteryyoon@gmail.com",
+    username: "petertest@gmail.com",
     nickname: "peter",
     password: "@ABCabc123",
     date_created: "2029-01-22T16:28:32.615Z"
   },
   {
     id: 2,
-    username: "hayoungsim@gmail.com",
+    username: "miketest@gmail.com",
     nickname: "mike",
     password: "@ABCabc123",
     date_created: "2029-01-22T16:28:32.615Z"
   },
   {
     id: 3,
-    username: "justinirhee@gmail.com",
+    username: "justintest@gmail.com",
     nickname: "justin",
     password: "@ABCabc123",
     date_created: "2029-01-22T16:28:32.615Z"
   },
   {
     id: 4,
-    username: "cfcardillo23@gmail.com",
+    username: "christest@gmail.com",
     nickname: "chris",
     password: "@ABCabc123",
     date_created: "2029-01-22T16:28:32.615Z"
   },
   {
     id: 5,
-    username: "derekgeryol@gmail.com",
+    username: "derektest@gmail.com",
     nickname: "derek",
     password: "@ABCabc123",
     date_created: "2029-01-22T16:28:32.615Z"
